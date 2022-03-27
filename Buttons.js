7e731f488283bc32ee348cdb60adfb9c054c2dae
Marius Birsan <100488307+MariusBirsan@users.noreply.github.com>
2022-03-27 17:50:24 +0300

    Update Buttons.js

--- Buttons.js
+++ Buttons.js
@@ -1,4 +1,4 @@
-var winningButton = 0;
+let winningButton = 0;
 
 function randomButton(totalB) {
     return Math.floor(Math.random() * totalB);
@@ -15,31 +15,30 @@ function isWinner(valueOfButton) {
     }
 }
 
-
 let totalButtons = 0;
 const maxValue = 20;
 
 function generateButtons() {
-    let input = document.getElementById("noOfButtons").value;
-    let value = parseInt(input);
-    if (input === "") {
-    	value = 0;
-    }
-	if (value > 20 || value < 1) {
-            message.innerHTML = "You need to enter a value bigger than 0 and smaller than 21!"
-    } else if (totalButtons < maxValue){
-        for (let i = 0; i < value && totalButtons < maxValue; ++i) {
-            let btn = document.createElement("button");
-            btn.addEventListener("click", isWinner);
-            btn.setAttribute("id", i);
-            btn.innerHTML = "Click Me";
-            document.body.appendChild(btn);
-            ++totalButtons;
+	let input = document.getElementById("noOfButtons").value;
+    	let value = parseInt(input);
+    	if (input === "") {
+    		value = 0;
+    	}	
+if (value > 20 || value < 1) {
+	message.innerHTML = "You need to enter a value bigger than 0 and smaller than 21!"
+} else if (totalButtons < maxValue){
+	for (let i = 0; i < value && totalButtons < maxValue; ++i) {
+        	let btn = document.createElement("button");
+           	btn.addEventListener("click", isWinner);
+            	btn.setAttribute("id", i);
+            	btn.innerHTML = "Click Me";
+            	document.body.appendChild(btn);
+            	++totalButtons;
         }
         if (totalButtons < maxValue) {
-            message.innerHTML = "Buttons were added! Check which one is the winner!"
+        	message.innerHTML = "Buttons were added! Check which one is the winner!"
         } else {
-            message.innerHTML = "You have reached to the maximum number of buttons! Check which button is the winner!"
+            	message.innerHTML = "You have reached to the maximum number of buttons! Check which button is the winner!"
         }
     }
     winningButton = randomButton(totalButtons);
